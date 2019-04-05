dbb2f8f0b3e0e6e54beff08ea390447577237e12
Kelvin Maina <thiongokelvin5@gmail.com>
2019-04-05 15:36:03 +0300

    Gien Njoro

--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,28 @@ Route::get('/items/report/{slug}', 'ItemsController@report')->name('report');
 //Auth Routes
 Auth::routes(['verify' => true]);
 
+// get clients country
+Route::get('/get-client-country', function(){ // getLocationInfoByIp
+    $client  = @$_SERVER['HTTP_CLIENT_IP'];
+    $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+    $remote  = @$_SERVER['REMOTE_ADDR'];
+    $result  = array('country'=>'', 'city'=>'');
+    if(filter_var($client, FILTER_VALIDATE_IP)){
+        $ip = $client;
+    }elseif(filter_var($forward, FILTER_VALIDATE_IP)){
+        $ip = $forward;
+    }else{
+        $ip = $remote;
+    }
+
+    $ip_data = @json_decode(file_get_contents("htts://www.geoplugin.net/json.gp?ip=".$ip));    
+    if($ip_data && $ip_data->geoplugin_countryName != null){
+        $result['country'] = $ip_data->geoplugin_countryCode;
+        $result['city'] = $ip_data->geoplugin_city;
+    }
+    return $result;
+});
+
 Route::get('/home', 'HomeController@index')->name('home');
 Route::group(['prefix' => 'admin', 'middleware' => ['auth', 'admin']], function(){
     

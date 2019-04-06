d6f567accfaca296f93b6648b1a275f4d1cc3b07
George Njoroge <georgenjoroge977@gmail.com>
2019-04-06 17:08:48 +0300

    UsersController conflict resolve

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -70,10 +70,10 @@ class UsersController extends Controller
             Session::flash('error', 'The email is already registered with us!');
             return redirect()->back();
         }
-        $slug = str_slug($request->name);
+        $slug = str_slug(ucwords($request->name));
         $check = User::withTrashed()->where('slug', $slug)->count();
         $user = User::create([
-            'name' => $request->name,
+            'name' => ucwords($request->name),
             'email' => $request->email,
             'phone' => $request->phone,
             'is_verified' => $request->is_verified,
@@ -108,10 +108,10 @@ class UsersController extends Controller
             else{
                 $type = 'ordinary';
             }
-            $slug = str_slug($request->name);
+            $slug = str_slug(ucwords($request->name));
             $check = User::withTrashed()->where('slug', $slug)->count();
             $admin = User::create([
-                'name' => $request->name,
+                'name' => ucwords($request->name),
                 'email'=> $request->email,
                 'type'=> $type,
                 'slug'=> $slug,
@@ -226,7 +226,7 @@ class UsersController extends Controller
                 }
                 $avatar_name = time() . $avatar->getClientOriginalName();
                 $avatar_new_name = 'uploads/users/' . $avatar_name;
-                $new_avatar = Image::make($avatar->getRealPath())->resize(172, 50);
+                $new_avatar = Image::make($avatar->getRealPath())->resize(500, 500);
                 $new_avatar->save(public_path($avatar_new_name));
                 $avatar = $avatar_new_name;
                 $user->avatar = $avatar;
@@ -236,13 +236,11 @@ class UsersController extends Controller
                 $user->phone = $request->phone;
             }
 
-           
-
             if(User::where('email', $request->email)->where('email','!=',$user->email)->count() > 0){
-               return redirect()->back()->with('error','Sorry The record already exists'); 
-            }  
+                return redirect()->back()->with('error','Sorry The record already exists'); 
+             }  
 
-             $user->name = $request->name;
+            $user->name = ucwords($request->name);
             $user->email = $request->email;
             if($user->type != 'user'){
                 if($request->supper == true){
@@ -266,10 +264,12 @@ class UsersController extends Controller
                     return redirect()->back();
                 }
             }
+
+
             $result = $user->save();
 
             if($result){
-                Session::flash('success', 'You successfully updated the admin profile.');
+                Session::flash('success', 'You successifully updated the admin profile.');
                 return redirect()->route('users.show', ['slug' => $slug]);
             }
 

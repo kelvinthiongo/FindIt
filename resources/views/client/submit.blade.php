04089b24a33c72c9c2ed33b0b843939c09fc20db
njoro <giennjoro@24seven.co.ke>
2019-07-22 16:16:51 +0300

    .

--- resources/views/client/submit.blade.php
+++ resources/views/client/submit.blade.php
@@ -3,7 +3,62 @@
 
 <body class="hold-transition skin-blue sidebar-mini">
     <div class="wrapper">
-        @include('layouts.header')
+        <header class="main-header">
+            <!-- Logo -->
+            <a href="/" target="_blank" class="logo">
+                <!-- logo for regular state and mobile devices -->
+                <span class="logo-lg"><img src="{{ asset('logo-white.png')}}" alt="JKUAT Logo" /></span>
+            </a>
+            <!-- Header Navbar: style can be found in header.less -->
+            <nav class="navbar navbar-static-top">
+                <div class="navbar-custom-menu">
+                    <ul class="nav navbar-nav">
+                        <li class="dropdown user user-menu">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                <img src="{{ asset(Auth::User()->avatar) }}" class="user-image" alt="User Image">
+                                <span class="hidden-xs"> {{ Auth::User()->name }}</span>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <!-- User image -->
+                                <li class="user-header">
+                                    <img src="{{ asset(Auth::User()->avatar) }}" class="img-circle" alt="User Image">
+
+                                    <p>
+                                        {{ Auth::User()-> name }} - Admin at FindIt
+                                        <small>Member since
+                                            {{ date('F d, Y', strtotime(Auth::User()->created_at)) }}</small>
+                                    </p>
+                                </li>
+                                <!-- Menu Body -->
+                                <!-- Menu Footer-->
+                                <li class="user-footer">
+                                    <div class="pull-left">
+                                        <a href="{{ route('users.show', ['slug' => Auth::user()->slug]) }}"
+                                            class="btn btn-default btn-flat">Profile</a>
+                                    </div>
+                                    <div class="pull-right">
+                                        <a href="{{ route('logout') }}" class="btn btn-default btn-flat" onclick="event.preventDefault();
+                                        document.getElementById('logout-form').submit();">
+                                            {{ __('Logout') }}
+                                        </a>
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                            style="display: none;">
+                                            @csrf
+                                        </form>
+                                        </a>
+
+                                    </div>
+                                </li>
+                            </ul>
+                        </li>
+                        <!-- Control Sidebar Toggle Button -->
+                        <li>
+                            <!-- <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a> -->
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
         <!-- Left side column. contains the logo and sidebar -->
 
         <!-- Content Wrapper. Contains page content -->

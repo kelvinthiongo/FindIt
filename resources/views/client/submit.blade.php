a577c0b88b081ee0e1dc7f5d405f9b4edf4e5528
kelvinthiongo <thiongokelvin5@gmail.com>
2019-09-26 20:15:27 +0300

    d

--- resources/views/client/submit.blade.php
+++ resources/views/client/submit.blade.php
@@ -327,10 +327,10 @@
     <!-- /.content-wrapper -->
     <footer class="main-footer">
         <div class="pull-right hidden-xs">
-            <b>Developed by </b><a href="https:24seven.co.ke">24 seven Developers </a>
+            <b>Developed by </b><a href="https://24seven.co.ke">24seven Developers </a>
         </div>
         <strong>Copyright &copy; {{ date('Y') }} <a href="https://jkuat.ac.ke"> JKUAT</a>.</strong> All rights
-        reserved. <span class="hidden-sm hidden-lg hidden-md"> | <b>Developed by </b><a href="https:24seven.co.ke">24 seven Developers </a></span>
+        reserved. <span class="hidden-sm hidden-lg hidden-md"> | <b>Developed by </b><a href="https://24seven.co.ke">24seven Developers </a></span>
     </footer>
 
     </div>

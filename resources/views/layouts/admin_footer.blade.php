a216da749c113bec0f4f11306f923653bf1d2afe
kelvinthiongo <thiongokelvin5@gmail.com>
2019-09-26 19:36:34 +0300

    d

--- resources/views/layouts/admin_footer.blade.php
+++ resources/views/layouts/admin_footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="main-footer">
     <div class="pull-right hidden-xs">
-        <b>Developed by: </b><a href="https://24seven.co.ke">24 seven Developers </a>
+        <b>Developed by: </b><a href="https://24seven.co.ke" target="_blank">24 seven Developers </a>
     </div>
     <strong>Copyright &copy; {{ date('Y') }} <a href="https://jkuat.ac.ke"> JKUAT</a>.</strong> All rights
-    reserved. <span class="hidden-sm hidden-lg hidden-md"> | <b>Developed by: </b><a href="https://24seven.co.ke">24 seven Developers </a></span>
+    reserved. <span class="hidden-sm hidden-lg hidden-md"> | <b>Developed by: </b><a href="https://24seven.co.ke" target="_blank">24 seven Developers </a></span>
 </footer>
